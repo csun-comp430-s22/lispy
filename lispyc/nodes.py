6547baaf1b0da76777f2a4af7c0ec732aa0f7738
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-02-19 14:28:33 -0800

    Parser: parse into an AST instead of a parse tree

---
+++ lispyc/nodes.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+
+import lark
+from lark import ast_utils
+
+__all__ = ("Node", "SExpression", "Atom", "List", "Program")
+
+
+class Node(ast_utils.Ast):
+    """Base class for all nodes of an abstract syntax tree (AST)."""
+
+
+@dataclass
+class SExpression(Node, ast_utils.WithMeta):
+    """A symbolic expression. The fundamental syntactic element of lispy."""
+
+    meta: lark.tree.Meta
+
+
+@dataclass
+class Atom(SExpression):
+    """An atomic S-expression; an atomic literal or a constant.
+
+    `value` contains the Python object the constant represents.
+    If the atom is instead an atomic literal, then it's a string representing the atom's name.
+    """
+
+    value: str | int | float | bool
+
+
+@dataclass
+class List(SExpression, ast_utils.AsList):
+    """An S-expression list.
+
+    `elements` holds a list of zero or more nodes representing the list's elements.
+    They are S-expressions which are contained within the list.
+    """
+
+    elements: list[SExpression]
+
+
+@dataclass
+class Program(Node, ast_utils.AsList):
+    """The top level of a lispy program.
+
+    `body` holds a list of zero or more direct child nodes.
+    They are S-expressions which are at the top level of the program.
+    """
+
+    body: list[SExpression]
--- lispyc/parser.py
+++ lispyc/parser.py
@@ -1,23 +1,32 @@
 import lark
-from lark import Lark, Transformer
+from lark import Lark, ast_utils
+
+from lispyc import nodes
 
 __all__ = ("parse",)
 
 
-class DiscardWhitespace(Transformer):
-    """Discard all whitespace tokens in a tree."""
+class AstTransformer(lark.Transformer):
+    """Transform a lispy parse tree (CST) into an AST."""
+
+    SIGNED_INT = int
+    FLOAT = float
+    BOOL = bool
+    LITERAL_ATOM = str
 
     def WS(self, _: lark.Token) -> lark.visitors.Discard:  # noqa: N802
         """Discard the current whitespace token."""
         return lark.visitors.Discard
 
 
-def parse(program: str) -> lark.Tree[lark.Token]:
-    """Parse a lispy program into a parse tree."""
+def parse(program: str) -> nodes.Program:
+    """Parse a lispy program into an AST."""
     with open("resources/grammar.lark", "r", encoding="utf8") as f:
         grammar = f.read()
 
     parser = Lark(grammar, start="program")
+    transformer = ast_utils.create_transformer(nodes, AstTransformer())
+
     tree = parser.parse(program)
 
-    return DiscardWhitespace().transform(tree)
+    return transformer.transform(tree)
