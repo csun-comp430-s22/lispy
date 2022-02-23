9cd12bd7d5b5d1321da1659f8e222e23a9db51cf
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-02-23 13:22:37 -0800

    Tests: ignore coverage for unknown package version

--- lispyc/__init__.py
+++ lispyc/__init__.py
@@ -2,5 +2,5 @@ from importlib import metadata
 
 try:
     __version__ = metadata.version("lispyc")
-except metadata.PackageNotFoundError:
+except metadata.PackageNotFoundError:  # pragma: no cover
     __version__ = "0.0.0+unknown"
