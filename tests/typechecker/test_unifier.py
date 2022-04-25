b00704fc5b62ef00e6266f049ca9b4a26f3c44bf
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-04-24 17:29:21 -0700

    Tests: add more unifier tests

--- tests/typechecker/test_unifier.py
+++ tests/typechecker/test_unifier.py
@@ -17,6 +17,13 @@ def test_identical_unknowns_unify(unifier):
     assert len(unifier._map) == 0
 
 
+@pytest.mark.parametrize("type_", [types.IntType, types.FloatType, types.BoolType])
+def test_identical_basic_types_unify(unifier, type_):
+    unifier.unify(type_(), type_())
+
+    assert len(unifier._map) == 0
+
+
 def test_unique_unknowns_unify(unifier):
     unknown_1 = types.UnknownType()
     unknown_2 = types.UnknownType()
@@ -28,3 +35,21 @@ def test_unique_unknowns_unify(unifier):
 
     assert representative_1 is representative_2
     assert (representative_1 is unknown_1) or (representative_2 is unknown_2)
+
+
+def test_identical_lists_unify(unifier):
+    list_1 = types.ListType(types.FloatType())
+    list_2 = types.ListType(types.FloatType())
+
+    unifier.unify(list_1, list_2)
+
+    assert len(unifier._map) == 0
+
+
+def test_identical_functions_unify(unifier):
+    func_1 = types.FunctionType((types.IntType(), types.BoolType()), types.FloatType())
+    func_2 = types.FunctionType((types.IntType(), types.BoolType()), types.FloatType())
+
+    unifier.unify(func_1, func_2)
+
+    assert len(unifier._map) == 0
