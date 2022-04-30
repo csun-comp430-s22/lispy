6afffdaafcce857772b8f2c31f8225b37c052da6
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-04-29 22:01:55 -0700

    Tests: test parsing variables and constants

---
+++ tests/test_parser.py
@@ -0,0 +1,27 @@
+import pytest
+
+from lispyc import nodes
+from lispyc.parser import parse
+
+CONSTANTS = [
+    ("1", 1),
+    ("-44", -44),
+    ("1.35", 1.35),
+    ("-2e-7", -2e-7),
+    ("false", False),
+    ("true", True),
+]
+
+
+@pytest.mark.parametrize("program", ["x", "y1", "z_A", "C"])
+def test_variable_parses(program):
+    result = parse(program)
+
+    assert result == nodes.Program((nodes.Variable(program),))
+
+
+@pytest.mark.parametrize(["program", "value"], CONSTANTS)
+def test_constant_parses(program, value):
+    result = parse(program)
+
+    assert result == nodes.Program((nodes.Constant(value),))
