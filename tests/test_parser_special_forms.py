d0e270a53b9b34d7b49fe1dd4096ef1a76a1d2f7
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-04-30 13:28:52 -0700

    Tests: test valid lambdas parse

---
+++ tests/test_parser_special_forms.py
@@ -0,0 +1,57 @@
+import pytest
+
+from lispyc import nodes
+from lispyc.nodes import ComposedForm, Constant
+from lispyc.nodes import FunctionParameter as Param
+from lispyc.nodes import Program, Variable, types
+from lispyc.parser import parse
+
+LAMBDA_PARAMS = [
+    ("(lambda () ())", (), nodes.List(())),
+    (
+        "(lambda ((int int) (bool bool) (float float)) (Y b 123))",
+        (
+            Param(Variable("int"), types.IntType()),
+            Param(Variable("bool"), types.BoolType()),
+            Param(Variable("float"), types.FloatType()),
+        ),
+        ComposedForm(Variable("Y"), (Variable("b"), Constant(123))),
+    ),
+    (
+        "(lambda ((a int)) (1.2 (a -44) false))",
+        (Param(Variable("a"), types.IntType()),),
+        ComposedForm(
+            Constant(1.2), (ComposedForm(Variable("a"), (Constant(-44),)), Constant(False))
+        ),
+    ),
+    (
+        "(lambda ((func (func ((list int) float) bool)) (list (list float))) (j__9e (true d)))",
+        (
+            Param(
+                Variable("func"),
+                types.FunctionType(
+                    (types.ListType(types.IntType()), types.FloatType()), types.BoolType()
+                ),
+            ),
+            Param(Variable("list"), types.ListType(types.FloatType())),
+        ),
+        ComposedForm(Variable("j__9e"), (ComposedForm(Constant(True), (Variable("d"),)),)),
+    ),
+    (
+        "(lambda ((f (func () (func (int) float)))) 1)",
+        (
+            Param(
+                Variable("f"),
+                types.FunctionType((), types.FunctionType((types.IntType(),), types.FloatType())),
+            ),
+        ),
+        Constant(1),
+    ),
+]
+
+
+@pytest.mark.parametrize(["program", "params", "body"], LAMBDA_PARAMS)
+def test_lambda_parses(program, params, body):
+    result = parse(program)
+
+    assert result == Program((nodes.Lambda(params, body),))
