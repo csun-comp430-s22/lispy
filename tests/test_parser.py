d02c60b061b4297ca2bc0d70177754af67a8d9e0
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-04-30 10:13:16 -0700

    Tests: test parsing composed forms

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,9 +1,10 @@
 import pytest
 
 from lispyc import nodes
+from lispyc.nodes import ComposedForm, Constant, Program, Variable
 from lispyc.parser import parse
 
-CONSTANTS = [
+CONSTANT_PARAMS = [
     ("1", 1),
     ("-44", -44),
     ("1.35", 1.35),
@@ -12,22 +13,62 @@ CONSTANTS = [
     ("true", True),
 ]
 
+COMPOSED_FORM_PARAMS = [
+    ("(myfunc 1 2)", Variable("myfunc"), (Constant(1), Constant(2))),
+    ("(1 false)", Constant(1), (Constant(False),)),
+    ("(true)", Constant(True), ()),
+    ("(0e0 C3_7e)", Constant(0e0), (Variable("C3_7e"),)),
+    (
+        "((retfunc a_1 -2e7) arg1 (x y Z))",
+        ComposedForm(Variable("retfunc"), (Variable("a_1"), Constant(-2e7))),
+        (
+            Variable("arg1"),
+            ComposedForm(Variable("x"), (Variable("y"), Variable("Z"))),
+        ),
+    ),
+    (
+        "((a (b 2) 1) (c (d (e 5) 4) 3) 70 80 90)",
+        ComposedForm(Variable("a"), (ComposedForm(Variable("b"), (Constant(2),)), Constant(1))),
+        (
+            ComposedForm(
+                Variable("c"),
+                (
+                    ComposedForm(
+                        Variable("d"), (ComposedForm(Variable("e"), (Constant(5),)), Constant(4))
+                    ),
+                    Constant(3),
+                ),
+            ),
+            Constant(70),
+            Constant(80),
+            Constant(90),
+        ),
+    ),
+]
+
 
 @pytest.mark.parametrize("program", ["x", "y1", "z_A", "C", "nil", "not", "float"])
 def test_variable_parses(program):
     result = parse(program)
 
-    assert result == nodes.Program((nodes.Variable(program),))
+    assert result == Program((Variable(program),))
 
 
-@pytest.mark.parametrize(["program", "value"], CONSTANTS)
+@pytest.mark.parametrize(["program", "value"], CONSTANT_PARAMS)
 def test_constant_parses(program, value):
     result = parse(program)
 
-    assert result == nodes.Program((nodes.Constant(value),))
+    assert result == Program((Constant(value),))
 
 
 def test_empty_list_parses():
     result = parse("()")
 
-    assert result == nodes.Program((nodes.List(()),))
+    assert result == Program((nodes.List(()),))
+
+
+@pytest.mark.parametrize(["program", "name", "args"], COMPOSED_FORM_PARAMS)
+def test_composed_form_parses(program, name, args):
+    result = parse(program)
+
+    assert result == Program((ComposedForm(name, args),))
