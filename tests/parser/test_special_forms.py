29702a35313a907f8539f9fe2f6c6781c55bf697
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-04-30 18:00:36 -0700

    Tests: generalise parser tests for special forms by arg count

--- tests/parser/test_special_forms.py
+++ tests/parser/test_special_forms.py
@@ -4,12 +4,12 @@ from lispyc import nodes
 from lispyc.nodes import ComposedForm, Constant, Program, Variable
 from lispyc.parser import parse
 
-VALID_LISTS = [
-    ("(list)", ()),
-    ("(list 1 false 4.2 -7e1)", (Constant(1), Constant(False), Constant(4.2), Constant(-7e1))),
-    ("(list (x false))", (ComposedForm(Variable("x"), (Constant(False),)),)),
+VALID_VAR_ARGS = [
+    ("($)", ()),
+    ("($ 1 false 4.2 -7e1)", (Constant(1), Constant(False), Constant(4.2), Constant(-7e1))),
+    ("($ (x false))", (ComposedForm(Variable("x"), (Constant(False),)),)),
     (
-        "(list (list (list 1 2) (list 3 4)) (list a b))",
+        "($ (list (list 1 2) (list 3 4)) (list a b))",
         (
             nodes.List(
                 (nodes.List((Constant(1), Constant(2))), nodes.List((Constant(3), Constant(4))))
@@ -20,39 +20,78 @@ VALID_LISTS = [
 ]
 
 
-VALID_CONS = [
-    ("(cons a b)", Variable("a"), Variable("b")),
+VALID_2_ARGS = [
+    ("($ a b)", Variable("a"), Variable("b")),
     (
-        "(cons (list a) (list b c))",
+        "($ (list a) (list b c))",
         nodes.List((Variable("a"),)),
         nodes.List((Variable("b"), Variable("c"))),
     ),
-    ("(cons (x 1) true)", ComposedForm(Variable("x"), (Constant(1),)), Constant(True)),
+    ("($ (x 1) true)", ComposedForm(Variable("x"), (Constant(1),)), Constant(True)),
 ]
 
-INVALID_CONS = [
-    "(cons)",
-    "(cons a)",
-    "(cons a b c)",
-    "(cons 1 2 a b c d true 17e-1)",
+INVALID_2_ARGS = [
+    "($)",
+    "($ a)",
+    "($ a b c)",
+    "($ 1 2 a b c d true 17e-1)",
+    "($ (x 1))",
+    "($ (list b c))",
+    "($ ($ 9 7))",
+]
+
+VALID_1_ARG = [
+    ("($ a)", Variable("a")),
+    ("($ 1)", Constant(1)),
+    ("($ false)", Constant(False)),
+    ("($ 1e-7)", Constant(1e-7)),
+    ("($ (x 1))", ComposedForm(Variable("x"), (Constant(1),))),
+    ("($ (list 1 2))", nodes.List((Constant(1), Constant(2)))),
+]
+
+INVALID_1_ARG = [
+    "($)",
+    "($ a b c)",
+    "($ 1 2 a b c d true 17e-1)",
+    "($ (x 1) 6)",
+    "($ false (list b c))",
+    "($ ($ y) (y 4))",
 ]
 
 
-@pytest.mark.parametrize(["program", "elements"], VALID_LISTS)
-def test_list_parses(program, elements):
-    result = parse(program)
+@pytest.mark.parametrize("node", [nodes.List])
+@pytest.mark.parametrize(["program", "args"], VALID_VAR_ARGS)
+def test_var_args_parses(program, args, node):
+    result = parse(program.replace("$", node.id))
 
-    assert result == Program((nodes.List(elements),))
-
-
-@pytest.mark.parametrize(["program", "car", "cdr"], VALID_CONS)
-def test_cons_parses(program, car, cdr):
-    result = parse(program)
-
-    assert result == Program((nodes.Cons(car, cdr),))
+    assert result == Program((node(args),))
 
 
-@pytest.mark.parametrize("program", INVALID_CONS)
-def test_invalid_cons_fails(program):
+@pytest.mark.parametrize("node", [nodes.Cons])
+@pytest.mark.parametrize(["program", "arg1", "arg2"], VALID_2_ARGS)
+def test_2_args_parses(program, arg1, arg2, node):
+    result = parse(program.replace("$", node.id))
+
+    assert result == Program((node(arg1, arg2),))
+
+
+@pytest.mark.parametrize("node", [nodes.Cons])
+@pytest.mark.parametrize("program", INVALID_2_ARGS)
+def test_invalid_2_args_fails(program, node):
     with pytest.raises(ValueError):  # noqa: PT011  # TODO: Use custom exception type.
-        parse(program)
+        parse(program.replace("$", node.id))
+
+
+@pytest.mark.parametrize("node", [nodes.Car, nodes.Cdr])
+@pytest.mark.parametrize(["program", "arg"], VALID_1_ARG)
+def test_1_arg_parses(program, arg, node):
+    result = parse(program.replace("$", node.id))
+
+    assert result == Program((node(arg),))
+
+
+@pytest.mark.parametrize("node", [nodes.Car, nodes.Cdr])
+@pytest.mark.parametrize("program", INVALID_1_ARG)
+def test_invalid_1_arg_fails(program, node):
+    with pytest.raises(ValueError):  # noqa: PT011  # TODO: Use custom exception type.
+        parse(program.replace("$", node.id))
