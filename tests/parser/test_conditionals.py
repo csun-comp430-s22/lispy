ca9eec5e21c1a3d736b2e38575d24915c06b50de
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-04-30 19:21:05 -0700

    Tests: test parsing of "cond" and "select" special forms

---
+++ tests/parser/test_conditionals.py
@@ -0,0 +1,150 @@
+import pytest
+
+from lispyc import nodes
+from lispyc.nodes import Branch, ComposedForm, Cond, Constant, Program, Variable
+from lispyc.parser import parse
+
+VALID_1_BRANCH_PROGRAMS = [
+    "($name$ $value$ ((x 7 y) (a b)) $default$)",
+    "(select $value$ ((cond (a b) 1.7) ()) $default$)",
+]
+
+VALID_PROGRAMS = [
+    "($name$ $value$ (a 7.9) (1 true) $default$)",
+    "($name$ $value$ ((x 7 y) (a b)) ((cond (a b) false) ()) $default$)",
+]
+
+VALID_NODES = [
+    (Branch(Variable("a"), Constant(7.9)), Branch(Constant(1), Constant(True))),
+    (
+        Branch(
+            ComposedForm(Variable("x"), (Constant(7), Variable("y"))),
+            ComposedForm(Variable("a"), (Variable("b"),)),
+        ),
+        Branch(Cond((Branch(Variable("a"), Variable("b")),), Constant(False)), nodes.List(())),
+    ),
+]
+
+VALID = list(zip(VALID_PROGRAMS, VALID_NODES))
+
+FORM_PROGRAMS = [
+    "()",
+    "(a)",
+    "(1)",
+    "(-7.9)",
+    "(true)",
+    "99",
+    "9.2",
+    "false",
+    "b2_1",
+    "nil",
+    "(list 23 false)",
+    "(x y 1)",
+]
+
+FORM_NODES = [
+    nodes.List(()),
+    ComposedForm(Variable("a"), ()),
+    ComposedForm(Constant(1), ()),
+    ComposedForm(Constant(-7.9), ()),
+    ComposedForm(Constant(True), ()),
+    Constant(99),
+    Constant(9.2),
+    Constant(False),
+    Variable("b2_1"),
+    Variable("nil"),
+    nodes.List((Constant(23), Constant(False))),
+    ComposedForm(Variable("x"), (Variable("y"), Constant(1))),
+]
+
+FORMS = list(zip(FORM_PROGRAMS, FORM_NODES))
+
+
+def replace(program, name: str, value: str, default: str) -> str:
+    return program.replace("$name$", name).replace("$value$", value).replace("$default$", default)
+
+
+@pytest.mark.parametrize(["default_program", "default"], FORMS)
+@pytest.mark.parametrize(["program", "branches"], VALID)
+def test_cond_parses(program, default_program, branches, default):
+    program = replace(program, "cond", "", default_program)
+
+    result = parse(program)
+
+    assert result == Program((nodes.Cond(branches, default),))
+
+
+@pytest.mark.parametrize(["default_program", "default"], FORMS)
+@pytest.mark.parametrize(["value_program", "value"], FORMS)
+@pytest.mark.parametrize(["program", "branches"], VALID)
+def test_select_parses(program, value_program, default_program, value, branches, default):
+    program = replace(program, "select", value_program, default_program)
+
+    result = parse(program)
+
+    assert result == Program((nodes.Select(value, branches, default),))
+
+
+@pytest.mark.parametrize("default", FORM_PROGRAMS)
+def test_cond_missing_branches_fails(default):
+    with pytest.raises(ValueError):  # noqa: PT011  # TODO: Use custom exception type.
+        parse(f"(cond {default})")
+
+
+@pytest.mark.parametrize("value", FORM_PROGRAMS)
+@pytest.mark.parametrize("program", VALID_PROGRAMS)
+def test_cond_with_value_fails(program, value):
+    program = replace(program, "cond", value, "false")
+
+    with pytest.raises(ValueError):  # noqa: PT011  # TODO: Use custom exception type.
+        parse(program)
+
+
+@pytest.mark.parametrize("program", VALID_1_BRANCH_PROGRAMS)
+def test_cond_missing_default_fails(program):
+    program = replace(program, "cond", "", "")
+
+    with pytest.raises(ValueError):  # noqa: PT011  # TODO: Use custom exception type.
+        parse(program)
+
+
+@pytest.mark.parametrize("invalid_branch", FORM_PROGRAMS)
+@pytest.mark.parametrize("program", VALID_PROGRAMS)
+def test_cond_invalid_branch_fails(program, invalid_branch):
+    program = replace(program, "cond", "", f"{invalid_branch} 12")
+
+    with pytest.raises(ValueError):  # noqa: PT011  # TODO: Use custom exception type.
+        parse(program)
+
+
+@pytest.mark.parametrize("default", FORM_PROGRAMS)
+@pytest.mark.parametrize("value", FORM_PROGRAMS)
+def test_select_missing_branches_fails(value, default):
+    with pytest.raises(ValueError):  # noqa: PT011  # TODO: Use custom exception type.
+        parse(f"(select {value} {default})")
+
+
+@pytest.mark.parametrize("program", VALID_1_BRANCH_PROGRAMS)
+def test_select_missing_value_and_default_fails(program):
+    program = replace(program, "select", "", "")
+
+    with pytest.raises(ValueError):  # noqa: PT011  # TODO: Use custom exception type.
+        parse(program)
+
+
+@pytest.mark.parametrize("value", FORM_PROGRAMS)
+@pytest.mark.parametrize("program", VALID_1_BRANCH_PROGRAMS)
+def test_select_missing_default_fails(program, value):
+    program = replace(program, "select", value, "")
+
+    with pytest.raises(ValueError):  # noqa: PT011  # TODO: Use custom exception type.
+        parse(program)
+
+
+@pytest.mark.parametrize("invalid_branch", FORM_PROGRAMS)
+@pytest.mark.parametrize("program", VALID_PROGRAMS)
+def test_select_invalid_branch_fails(program, invalid_branch):
+    program = replace(program, "select", "a", f"{invalid_branch} 12")
+
+    with pytest.raises(ValueError):  # noqa: PT011  # TODO: Use custom exception type.
+        parse(program)
