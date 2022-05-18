87b22aea88fdcc81c8769944dae8921e04a16a26
MarkKoz <1515135+MarkKoz@users.noreply.github.com>
2022-05-18 11:45:58 -0700

    Tests: test typechecking constants

---
+++ tests/typechecker/test_checker.py
@@ -0,0 +1,24 @@
+import pytest
+
+from lispyc import nodes
+from lispyc.parser import parse
+from lispyc.typechecker import TypeChecker
+
+CONSTANTS = [
+    ("120", nodes.IntType()),
+    ("-40", nodes.IntType()),
+    ("0", nodes.IntType()),
+    ("12e35", nodes.FloatType()),
+    ("-1e2", nodes.FloatType()),
+    ("489e-29", nodes.FloatType()),
+    ("true", nodes.BoolType()),
+    ("false", nodes.BoolType()),
+]
+
+
+@pytest.mark.parametrize(["program", "type_"], CONSTANTS)
+def test_constant_typechecks(program, type_):
+    program_node = parse(program)
+    result = list(TypeChecker.check_program(program_node))
+
+    assert result == [type_]
